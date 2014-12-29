1063e82df1f5b8914af89914d8cf7004d7c9b842
dharanimanne <dharani.manne@gmail.com>
2014-12-29 14:40:10 +0530

    categories file added

---
+++ categories.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Short description for categories.php
+ *
+ * @package categories
+ * @author root <root@students>
+ * @version 0.1
+ * @copyright (C) 2014 root <root@students>
+ * @license MIT
+ */
+
+$categories = array();
+array_push("reminder","academic","hostel","fee","exam","sports","lecture");
+
+?>
+
