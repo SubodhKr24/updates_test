f090f7fe001e59c2321b118317e1c3d649f3a972
dharanimanne <dharani.manne@gmail.com>
2014-12-29 14:47:09 +0530

    reminder static class complete

---
+++ reminder.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Short description for reminder.php
+ *
+ * @package reminder
+ * @author root <root@students>
+ * @version 0.1
+ * @copyright (C) 2014 root <root@students>
+ * @license MIT
+ */
+
+class Reminder {
+  public $string;
+  boolean $result;
+
+  public function check($string) {
+  }
+}
+
+?>
+
