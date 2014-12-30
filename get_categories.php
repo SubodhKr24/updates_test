06465b013aaa1d080a0eda690a17e85b8754e48f
dharanimanne <dharani.manne@gmail.com>
2014-12-30 21:55:06 +0530

    structure formed

--- get_categories.php
+++ get_categories.php
@@ -9,9 +9,31 @@
  * @license MIT
  */
 
+//Initializing categories array, containing the categories along with their keys.
 $categories = array();
-$myfile = fopen("categories.php","r");
-array_push("reminder","art","missing","academic","hostel","fee","exam","sports","lecture");
+
+//open file categories to read all the categories from there.
+$myfile = fopen("categories","r");
+
+//read everything from the file into the variable for further processing.
+$filecontents = fread($myfile,filesize("categories"));
+
+//splitting the file contents based on line break. Each new line indicates a new category.
+$components = explode(PHP_EOL,$filecontents);
+
+//var_dump($components);
+//array_push("reminder","art","missing","academic","hostel","fee","exam","sports","lecture");
+
+//from each category, separate out the key value pairs and push to the categories array.
+for ($i = 0; $i < sizeof($components); $i++) {
+  if($components[$i] != "") {
+    $intermediate = preg_split("/=/",$components[$i]);
+    array_push($categories,$intermediate);
+  }
+  else {
+    continue;
+  }
+}
 
 ?>
 
